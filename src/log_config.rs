4db72afb00d70b81cf9603fa64b27afef14b5ff8
hitol <1126031033@qq.com>
2023-11-22 14:07:45 +0800

    tracing

--- src/log_config.rs
+++ src/log_config.rs
@@ -1,31 +1,35 @@
-use chrono::{FixedOffset, Utc};
-use env_logger::{Builder, Target};
-use std::fs::OpenOptions;
-use std::io::Write;
+use time::macros::{format_description, offset};
+use tracing_subscriber::fmt::time::{FormatTime, OffsetTime};
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+use tracing_subscriber::EnvFilter;
+
+// 自定义时间格式化
+struct LocalTimer;
+const fn east8() -> Option<chrono::FixedOffset> {
+    chrono::FixedOffset::east_opt(8 * 3600)
+}
+
+impl FormatTime for LocalTimer {
+    fn format_time(&self, w: &mut tracing_subscriber::fmt::format::Writer<'_>) -> std::fmt::Result {
+        let now = chrono::Utc::now().with_timezone(&east8().unwrap());
+        write!(w, "{}", now.format("%Y-%m-%d %H:%M:%SS"))
+    }
+}
 
 pub fn init_log() {
-    // env_logger::init();
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open("app.log")
-        .expect("Failed to open log file");
-    let target = Box::new(file);
+    let time_fmt =
+        format_description!("[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]");
+    let timer = OffsetTime::new(offset!(+8), time_fmt);
 
-    let mut builder = Builder::from_default_env();
-    builder
-        .format(|buf, record| {
-            let timestamp = Utc::now().with_timezone(&FixedOffset::east(8 * 3600));
-            writeln!(
-                buf,
-                "{} [{}] [{}] {}",
-                timestamp.format("%Y-%m-%d %H:%M:%S"),
-                record.level(),
-                record.module_path().unwrap(),
-                record.args()
-            )
-        })
-        .target(Target::Pipe(target))
+    let appender = tracing_appender::rolling::daily("log/", "app.log");
+    let stdout = std::io::stdout.with_max_level(tracing::Level::INFO);
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_timer(timer)
+        // .with_timer(LocalTimer)
+        .with_writer(stdout.and(appender))
+        .with_line_number(true)
+        .with_thread_ids(true)
         .init();
 }
